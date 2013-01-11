3e656c20448978757ab4e902ce5cb26fc09f68fe
Dominic Rodger <dominicrodger+github@gmail.com>
2013-01-11 06:56:21 +0000

    Simpler name for the Fabric task

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric.api import env, hosts, run, cd, hide, prefix
 env.use_ssh_config = True
 env.hosts = ['dmr', ]
 
-def deploy_blog_changes():
+def deploy():
     with cd('~/dominicrodger.com'):
         run("git pull")
         with prefix('source .env/bin/activate'):
