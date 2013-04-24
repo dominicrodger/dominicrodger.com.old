8d61d93b977ea0571a13f375e59fa66b2a81c2da
Dominic Rodger <dominicrodger+github@gmail.com>
2013-04-24 23:32:21 +0100

    Trying out Digital Ocean

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,12 @@
 from fabric.api import env, run, cd, prefix, local
 
 env.use_ssh_config = True
-env.hosts = ['dmr', ]
+env.hosts = ['scuba', ]
 
 def deploy():
     local("git push")
-    with cd('~/dominicrodger.com'):
+    with cd('/var/www/dominicrodger.com'):
         run("git pull")
-        with prefix('source .env/bin/activate'):
+        with prefix('source env/bin/activate'):
             run('pip install -r requirements.txt')
             run('make')
